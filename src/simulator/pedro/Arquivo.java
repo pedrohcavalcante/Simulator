0505a99407a475df546b25e4e2aa00044ff597f1
SINFO <SINFO@10.3.131.215>
2016-10-04 17:35:40 -0300

    First try

---
+++ src/simulator/pedro/Arquivo.java
@@ -0,0 +1,28 @@
+package simulator.pedro;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+    public class Arquivo {
+    	
+    	public static void main(String [] args) throws FileNotFoundException{
+    		String line;
+    		File archive = new File ("C:\\Users\\SINFO\\workspace\\Arquivo\\src"
+    				+ "\\arquivo\\thunder.txt");
+    		if (archive.exists()){
+    		System.out.println("File founded on system");
+    		try(BufferedReader buffer_reader = new BufferedReader(new FileReader(archive))){
+    			while ((line = buffer_reader.readLine()) != null){
+    				
+    				System.out.println(line.split(","));
+    			}
+    		} catch (IOException e) {
+				e.printStackTrace();
+			}
+    			
+    		}
+    	}
+    }
