a3c2cb5093674a58e630a526b440e843c0f1f740
pedrohbcavalcante <pedrinho.sjs@ufrn.edu.br>
2016-10-04 23:10:23 -0300

    classe auxiliar para imprimir coisas.

---
+++ src/simulator/pedro/prints_methods.java
@@ -0,0 +1,9 @@
+package simulator.pedro;
+
+public class prints_methods {
+	public void print(String[] parts){
+		for (int i = 0; i < parts.length; i++){
+			System.out.println(parts[i]);
+		}
+	}
+}
